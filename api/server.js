7798a980409fd3a578a2e7fcc8262315f5b6b799
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-23 08:19:30 -0500

    ST:BE:DEV Added cors, helmet and update package.json
    Yarn added cors, helmet, bcryptjs, jsonwebtoken. Updated server.js to use cors and helmet

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(cors());
 
 server.get("/", async (req, res) => {
   res.status(200).json({ message: "The sleep tracker api is up!" });
