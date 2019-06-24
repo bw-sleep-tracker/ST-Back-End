7295a9e6017df178eea59e5e95129e736ddfd9c9
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-24 13:22:37 -0500

    ST:BE:HEROKU Updated migration file for Foreign Key
    Updated the migration file for foreign key constraint from year

--- data/migrations/20190623062521_tracker.js
+++ data/migrations/20190623062521_tracker.js
@@ -54,7 +54,7 @@ exports.up = function(knex) {
       .integer("year")
       .notNullable()
       .unsigned()
-      .references("id")
+      .references("year")
       .inTable("years")
       .onDelete("CASCADE")
       .onUpdate("CASCADE");
