3e1e1f22c23bb92153323e2a99c7a775744820a6
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-26 09:39:08 -0500

    ST:BE:TC Test case for add and find in user model completed

---
+++ models/UserModel.test.js
@@ -0,0 +1,62 @@
+const UserModel = require("./UserModel");
+const db = require("../data/dbconfig");
+const bcrypt = require("bcryptjs");
+
+describe("TS2: TESTING USERMODEL.JS", () => {
+  beforeEach(async () => {
+    await db("recommendation").truncate();
+    await db("tracker").truncate();
+    await db("users").truncate();
+  });
+  describe("TS2.1:: TEST ADD USER", () => {
+    it("TC2.1.1: Test to add a user - check length", async () => {
+      const user = {
+        username: "g3ram",
+        password: bcrypt.hashSync("password", 10),
+        email: "g3ram@verizon.net",
+        first_name: "Tester_firstName",
+        last_name: "Tester_lastName"
+      };
+      await UserModel.add(user);
+      const users = await db("users");
+      expect(users).toHaveLength(1);
+    });
+
+    it("TC2.1.2: Test to add a user - check username", async () => {
+      const user = {
+        username: "g3ram",
+        password: bcrypt.hashSync("password", 10),
+        email: "g3ram@verizon.net",
+        first_name: "Tester_firstName",
+        last_name: "Tester_lastName"
+      };
+      await UserModel.add(user);
+      const users = await db("users").first();
+      expect(users.username).toBe("g3ram");
+    });
+  });
+
+  describe("TS2.2:: GET ALL USERS (find)", () => {
+    it("TC2.2.1: Test to add a user", async () => {
+      let user = {
+        username: "g3ram",
+        password: bcrypt.hashSync("password", 10),
+        email: "g3ram@verizon.net",
+        first_name: "Tester_firstName",
+        last_name: "Tester_lastName"
+      };
+      await UserModel.add(user);
+
+      user = {
+        username: "joshc",
+        password: bcrypt.hashSync("password", 10),
+        email: "josh@verizon.net",
+        first_name: "Tester_firstName",
+        last_name: "Tester_lastName"
+      };
+      await UserModel.add(user);
+      const users = await db("users");
+      expect(users).toHaveLength(2);
+    });
+  });
+});
